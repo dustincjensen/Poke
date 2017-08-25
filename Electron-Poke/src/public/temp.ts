f7ada62005e1a77f0808ae044473680948146f21
Dustin Jensen <dustincjensen@gmail.com>
2017-08-25 01:36:34 -0600

    Everything should have waited for document to be ready.

--- Electron-Poke/src/public/temp.ts
+++ Electron-Poke/src/public/temp.ts
@@ -9,29 +9,29 @@ class Temp {
 
             let electronVersion = document.getElementById('electronVersion') as HTMLParagraphElement;
             electronVersion.innerText = process.versions.electron;
-        };
 
-        let ipcRenderer = require('electron').ipcRenderer;
-        ipcRenderer.on('new-message', (event, args) => {
-            console.log('New Message', args);
-            let obj = JSON.parse(args);
-            let messages = document.getElementById('messages');
-            let li = document.createElement('li');
-            li.appendChild(document.createTextNode(`${obj.contact.name} :: ${obj.contact.phoneNumber} :: ${obj.message}`));
-            messages.appendChild(li);
-        });
+            let ipcRenderer = require('electron').ipcRenderer;
+            ipcRenderer.on('new-message', (event, args) => {
+                console.log('New Message', args);
+                let obj = JSON.parse(args);
+                let messages = document.getElementById('messages');
+                let li = document.createElement('li');
+                li.appendChild(document.createTextNode(`${obj.contact.name} :: ${obj.contact.phoneNumber} :: ${obj.message}`));
+                messages.appendChild(li);
+            });
 
-        let submitButton = document.getElementById('submitButton') as HTMLInputElement;
-        let textBox = document.getElementById('msgToAndroid') as HTMLInputElement;
-        submitButton.onclick = function () {
-            let obj = {
-                contact: {
-                    phoneNumber: 'FILL_IN_TO_SEND_DEBUG_MESSAGE'
-                },
-                message: textBox.value
+            let submitButton = document.getElementById('submitButton') as HTMLInputElement;
+            let textBox = document.getElementById('msgToAndroid') as HTMLInputElement;
+            submitButton.onclick = function () {
+                let obj = {
+                    contact: {
+                        phoneNumber: 'FILL_IN_TO_SEND_DEBUG_MESSAGE'
+                    },
+                    message: textBox.value
+                };
+                textBox.value = '';
+                ipcRenderer.send('newMessageForAndroid', JSON.stringify(obj));
             };
-            textBox.value = '';
-            ipcRenderer.send('newMessageForAndroid', JSON.stringify(obj));
         };
     }
 }
