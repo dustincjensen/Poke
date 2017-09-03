b38a4d8ec8d003f035b5a09d3127b0c711e5100a
Dustin Jensen <dustincjensen@gmail.com>
2017-09-03 14:21:34 -0600

    Sets up a no messages watermark.

--- Electron-Poke/src/public/app/example/example.component.ts
+++ Electron-Poke/src/public/app/example/example.component.ts
@@ -30,58 +30,58 @@ export class ExampleComponent extends ElectronComponent implements OnInit, After
 
         // TMP
         this.displayName = 'Dave Grohl';
-        this.messages.push(
-            {
-                contact: {
-                    id: 784,
-                    phoneNumber: '+19695553215',
-                    name: 'Dave Grohl',
-                    isSelf: false
-                },
-                message: 'Hey, how are you doing today friend?',
-                time: Date.now() - (36 * 60 * 1000)
-            },
-            {
-                contact: {
-                    id: 784,
-                    phoneNumber: '+19695553215',
-                    name: 'Dave Grohl',
-                    isSelf: false
-                },
-                message: 'I was wondering what you are up to today?',
-                time: Date.now() - (35 * 60 * 1000)
-            },
-            {
-                contact: {
-                    id: 0,
-                    phoneNumber: null,
-                    name: 'Me',
-                    isSelf: true
-                },
-                message: 'Hey Dave. I\'m doing pretty well, thanks for asking',
-                time: Date.now() - (30 * 60 * 1000)
-            },
-            {
-                contact: {
-                    id: 0,
-                    phoneNumber: null,
-                    name: 'Me',
-                    isSelf: true
-                },
-                message: 'I\'m not up to much, did you have something in mind? I would totally be up for some food or something.',
-                time: Date.now() - (30 * 60 * 1000)
-            },
-            {
-                contact: {
-                    id: 784,
-                    phoneNumber: '+19695553215',
-                    name: 'Dave Grohl',
-                    isSelf: false
-                },
-                message: 'How about we get together and jam?',
-                time: Date.now() - (29 * 60 * 1000)
-            }
-        );
+        // this.messages.push(
+        //     {
+        //         contact: {
+        //             id: 784,
+        //             phoneNumber: '+19695553215',
+        //             name: 'Dave Grohl',
+        //             isSelf: false
+        //         },
+        //         message: 'Hey, how are you doing today friend?',
+        //         time: Date.now() - (36 * 60 * 1000)
+        //     },
+        //     {
+        //         contact: {
+        //             id: 784,
+        //             phoneNumber: '+19695553215',
+        //             name: 'Dave Grohl',
+        //             isSelf: false
+        //         },
+        //         message: 'I was wondering what you are up to today?',
+        //         time: Date.now() - (35 * 60 * 1000)
+        //     },
+        //     {
+        //         contact: {
+        //             id: 0,
+        //             phoneNumber: null,
+        //             name: 'Me',
+        //             isSelf: true
+        //         },
+        //         message: 'Hey Dave. I\'m doing pretty well, thanks for asking',
+        //         time: Date.now() - (30 * 60 * 1000)
+        //     },
+        //     {
+        //         contact: {
+        //             id: 0,
+        //             phoneNumber: null,
+        //             name: 'Me',
+        //             isSelf: true
+        //         },
+        //         message: 'I\'m not up to much, did you have something in mind? I would totally be up for some food or something.',
+        //         time: Date.now() - (30 * 60 * 1000)
+        //     },
+        //     {
+        //         contact: {
+        //             id: 784,
+        //             phoneNumber: '+19695553215',
+        //             name: 'Dave Grohl',
+        //             isSelf: false
+        //         },
+        //         message: 'How about we get together and jam?',
+        //         time: Date.now() - (29 * 60 * 1000)
+        //     }
+        // );
         // TMP
 
         this.registerIpcRendererMethod('new-message', this._handleNewMessage);
